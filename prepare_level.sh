c66c62552b6c666b47c0a5901062c790531ad5a0
valentin <valentin@capiau.eu>
2024-02-01 15:24:20 +0100

    code rearranging (multi year support) - update prepare level script

--- prepare_level.sh
+++ prepare_level.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-LEVEL=$(printf "%02d" $1)
-[ -e ${LEVEL}.py ] || cp "00.py" ${LEVEL}.py
-[ -e fixtures/level_${LEVEL}_part_1.txt ] || cp fixtures/level_00_part_1.txt fixtures/level_${LEVEL}_part_1.txt
-[ -e fixtures/level_${LEVEL}_part_2.txt ] || cp fixtures/level_00_part_2.txt fixtures/level_${LEVEL}_part_2.txt
+YEAR=$1
+LEVEL=$(printf "%02d" $2)
+
+mkdir -p ${YEAR}/fixtures
+[ -e ${YEAR}/${LEVEL}.py ] || cp "00.py" ${YEAR}/${LEVEL}.py
+[ -e ${YEAR}/fixtures/level_${LEVEL}_part_1.txt ] || cp level_00_part_1.txt ${YEAR}/fixtures/level_${LEVEL}_part_1.txt
+[ -e ${YEAR}/fixtures/level_${LEVEL}_part_2.txt ] || cp level_00_part_2.txt ${YEAR}/fixtures/level_${LEVEL}_part_2.txt
